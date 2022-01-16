60d1c0865348bb242f649a48c930a16591c977ce
Arthur Monteiro Falcão <95629281+Arthur-Falcao@users.noreply.github.com>
2022-01-15 23:36:16 -0300

    Add files via upload

---
+++ Contador/script.js
@@ -0,0 +1,20 @@
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+let count = 0;
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+	}
+	
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+	}
+
+
+
+
+
+
+
+
